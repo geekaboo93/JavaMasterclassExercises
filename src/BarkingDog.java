6d3cbbf6bede426381679894b84a5e076f35df49
geekaboo93 <doub1ea1006@gmail.com>
2019-09-25 23:58:18 +0800

    Exercise- Barking Dog

---
+++ src/BarkingDog.java
@@ -0,0 +1,26 @@
+public class BarkingDog {
+    public static void main(String[] args) {
+        System.out.println(shouldWakeUp(true, 1));
+        System.out.println(shouldWakeUp(false, 2));
+        System.out.println(shouldWakeUp(true, 8));
+        System.out.println(shouldWakeUp(true, -1));
+    }
+
+    /**
+     *
+     * @param barking
+     * @param hourOfDay
+     * @return
+     */
+    public static boolean shouldWakeUp(boolean barking, int hourOfDay) {
+        boolean result = false;
+        if(hourOfDay < 0 || hourOfDay > 23) {
+            return result;
+        }
+
+        if(barking && (hourOfDay < 8 || hourOfDay > 22)) {
+            result = true;
+        }
+        return result;
+    }
+}
