bd6fb3a545ff602cb3867b4d7d4897c6ba6a383c
geekaboo93 <doub1ea1006@gmail.com>
2019-12-30 20:15:46 +0800

    GCD

---
+++ src/GreatestCommonDivisor.java
@@ -0,0 +1,21 @@
+public class GreatestCommonDivisor {
+    public static void main(String[] args) {
+        System.out.println(getGreatestCommonDivisor(25,15));
+        System.out.println(getGreatestCommonDivisor(12,30));
+        System.out.println(getGreatestCommonDivisor(9,18));
+        System.out.println(getGreatestCommonDivisor(81,153));
+    }
+
+    public static int getGreatestCommonDivisor(int first , int second) {
+        if(first < 10 || second < 10)
+            return -1;
+        while(second != 0) {
+            if(first > second) {
+                first = first - second;
+            } else {
+                second = second - first;
+            }
+        }
+        return first;
+    }
+}
