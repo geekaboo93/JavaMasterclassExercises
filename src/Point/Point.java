17085bebea31231ec4d52e6c0bb111f0573722d4
geekaboo93 <doub1ea1006@gmail.com>
2020-01-02 17:00:56 +0800

    refs #CE-32, Point

---
+++ src/Point/Point.java
@@ -0,0 +1,43 @@
+package Point;
+
+public class Point {
+    private int x;
+    private int y;
+
+    public Point() {
+
+    }
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public double distance() {
+        return Math.sqrt(x * x + y * y);
+    }
+
+    public double distance(int x, int y) {
+        return Math.sqrt((x - this.x) * (x - this.x) + (y - this.y) * (y - this.y));
+    }
+
+    public double distance(Point p) {
+        return Math.sqrt((p.getX() - this.x) * (p.getX() - this.x) + (p.getY() - this.y) * (p.getY() - this.y));
+    }
+}
